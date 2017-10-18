5937e4a549d208db1629a6a905346bbc2acc6119
Benjamin Watto <wattob@aldenv142.allegheny.edu>
2017-10-18 11:31:31 -0400

    Committing Lab 6

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,22 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
+
+/** This class accepts user input and displays an output.
+  *This program manipulates DNA using G, A, T, and C
+  */
 
 public class ManipulateDna {
 
+  /** The user inputs DNA and an output is produced.
+   * Strings have complements and sometimes undergo mutations.
+   */
+
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Benjamin Watto " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,16 +30,45 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
+    String dnaString = scanner.nextLine();
+    System.out.println("Okay, I am going to manipulate the DNA string, " + dnaString);
+    System.out.println();
+    System.out.println("Enter a string containing only C, G, T, and A: " + dnaString);
 
     // Step Two: Compute the complement of the DNA String
+    String compDna = dnaString.replace('a','T');
+    compDna = compDna.replace('t','A');
+    compDna = compDna.replace('c','G');
+    compDna = compDna.replace('g','C');
+    System.out.println("Complement of " + dnaString + " is " + compDna);
 
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    String dnaLetters = "cgta";
+    Random variable = new Random();
+    char letter = dnaLetters.charAt(variable.nextInt(4));
+    int randomInsertionIndex = variable.nextInt(dnaString.length());
+    String mutation1 = dnaString.substring(0, randomInsertionIndex)
+        + letter + dnaString.substring(randomInsertionIndex, dnaString.length());
+    System.out.println("Inserting " + letter + " at position " 
+        + randomInsertionIndex + " gives " + mutation1);
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
+    int randomRemoveIndex = variable.nextInt(dnaString.length());
+    String mutation2 = dnaString.substring(0, randomRemoveIndex);
+    String mutation3 = dnaString.substring(randomRemoveIndex + 1, dnaString.length());
+    System.out.println("Deleting from position " + randomRemoveIndex 
+        + " gives " + mutation2 + mutation3);
 
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
+    int randomInsertionIndexTwo = variable.nextInt(dnaString.length());
+    String mutation4 = dnaString.substring(0, randomInsertionIndexTwo);
+    String mutation5 = dnaString.substring(randomInsertionIndexTwo + 1, dnaString.length());
+    System.out.println("Changing position " + randomInsertionIndexTwo + " gives " 
+        + mutation4 + letter + mutation5);
 
-    // Step Six: Display a final thankyou message
+    // Step Six: Display a final thank you message
+    System.out.println();
+    System.out.println("Thank you for using the ManipulateDNA program.");
+    System.out.println("Have a great day!");
 
   }
 
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,22 @@
 # Reflection by
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to write a file called `ManipulateDNA`. Overall, this lab
+has been the most difficult. We were given a two week period to work on it
+which helped a great deal. Early on, we made a lot of progress by adding
+javadoc comments and the println statements. After that we added the scanner to
+read in the inputs. One key thing we added was an import for the random util
+which we learned has to be in alphabetical order.
+
+Step two was difficult to understand, but eventually we realized capital letter
+and lowercase letters are treated as two different things. We took advantage of
+that to replace letter with their complement. Soon after we began to work on
+step three, which was by far the most time consuming. We had to create a random
+variableand a char for the letter being changed. An int for the insertion Index
+was also made using the guide on the white board. Following that we used string
+muations to represent the fragments of the strings.
+
+After step three, we moved on to deleting a randomly chosen point, which was
+much easier because we had done something similar in the step above. By
+following a similar writing style we were able to randomly choose insertion
+points and use more mutations for steps four and five. This lab has been
+difficult, but a great learning experience.
